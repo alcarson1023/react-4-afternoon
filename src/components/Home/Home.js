5ac3131f023ab8fb0b9dcab0399dcd1e8fee56c1
Alexander Carson <alexandercarson@Alexanders-MacBook-Pro.local>
2018-08-21 16:03:00 -0500

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="box">
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
+        <Link to="/classlist/MATH1010"><button className='btn'>
+          <p>Math 1010</p>
+        </button></Link>
+        <Link to="/classlist/ENG2010"><button className='btn'>
+          <p>English 2010</p>
+        </button></Link>
+        <Link to="/classlist/BIO2020"><button className='btn'>
+          <p>Biology 2020</p>
+        </button></Link>
       </div>
-    );
+    )
   }
 }
